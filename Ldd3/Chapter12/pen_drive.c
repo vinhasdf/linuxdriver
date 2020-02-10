ce4fdb405fc8996160b927a7a2061983b1925d7e
vinhasdf <vinhasdf12@gmail.com>
2020-02-10 21:03:35 +0700

    abc

--- Ldd3/Chapter12/pen_drive.c
+++ Ldd3/Chapter12/pen_drive.c
@@ -8,6 +8,8 @@ MODULE_AUTHOR("Vinh");
 #define PEN_VENDOR	0x1908
 #define PEN_DEVID	0x0226
 
+struct usb_device *pen_device;
+
 struct usb_device_id pend_ids[] = {
 	{ USB_DEVICE(PEN_VENDOR, PEN_DEVID) },
 	{}
@@ -17,7 +19,26 @@ MODULE_DEVICE_TABLE(usb, pend_ids);
 
 int pend_probe(struct usb_interface *intf, const struct usb_device_id *id)
 {
-	printk(KERN_INFO "Pen drive (%04X:%04X) plugged\n", id->idVendor, id->idProduct);
+	int i;
+	struct usb_host_interface *id_if;
+	struct usb_endpoint_descriptor *endpoint;
+	id_if = intf->cur_altsetting;
+
+	printk(KERN_INFO "Pen i/f %d now probed: (%04X:%04X)\n", 
+		id_if->desc.bInterfaceNumber,id->idVendor, id->idProduct);
+	printk(KERN_INFO "Number of endpoint: %d\n", id_if->desc.bNumEndpoints);
+	printk(KERN_INFO "Interface class: %02X\n", id_if->desc.bInterfaceClass);
+
+	for(i = 0; i < id_if->desc.bNumEndpoints; i++)
+	{
+		endpoint = &id_if->endpoint[i].desc;
+		printk(KERN_INFO "ED[%d] Address: %02X\n", i, endpoint->bEndpointAddress);
+		printk(KERN_INFO "ED[%d] Attribute: %02X\n", i, endpoint->bmAttributes);
+		printk(KERN_INFO "ED[%d] Max packet size: %d\n", i, endpoint->wMaxPacketSize);
+	}
+
+	pen_device = interface_to_usbdev(intf);
+
 	return 0;
 }
 
@@ -45,7 +66,7 @@ static int pen_init(void)
 static void pen_exit(void)
 {
 	usb_deregister(&pen_fops);
-	printk(KERN_INFO "Good bye pend drivers");
+	printk(KERN_INFO "Good bye pend drivers\n");
 }
 
 module_init(pen_init);
