7adfb41cf4ba9722e6a47c40b295a5cdaeb0e039
vinhasdf <vinhasdf12@gmail.com>
2020-02-27 20:46:19 +0700

    lol

---
+++ Ldd3/Chapter15/simple_block.c
@@ -0,0 +1,76 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/genhd.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Vinhasdf");
+
+#define SIMPLE_BLOCK_SECTOR_SIZE    512
+#define SIMPLE_BLOCK_NUM_SECTOR     1000
+
+struct gendisk *simpledisk;
+int major;
+
+static struct block_device_operations blfops = {
+    .owner = THIS_MODULE,
+};
+
+static void bl_request(struct request_queue *q)
+{
+
+}
+
+static int block_init(void)
+{
+    int ret;
+    major = register_blkdev(0, "simple_block"); // Dynamic allocated
+    if(major < 0)
+    {
+        printk(KERN_INFO "Cannot alloc major for simple block\n");
+        return major;
+    }
+    simpledisk = alloc_disk(1);
+    if(simpledisk == NULL)
+    {
+        printk(KERN_INFO "Cannot alloc gendisk struct\n");
+        ret = -ENOMEM;
+        goto out1;
+    }
+
+    simpledisk->major = major;
+    simpledisk->first_minor = 0;
+    simpledisk->minors = 1;
+    simpledisk->fops = &blfops;
+    strncpy(simpledisk->disk_name, "simple_block", sizeof(simpledisk->disk_name));
+    simpledisk->queue = blk_init_queue(bl_request, NULL);
+    if(simpledisk->queue == NULL)
+    {
+        ret = -ENOMEM;
+        printk(KERN_INFO "Cannot alloc queue struct\n");
+        goto out2;
+    }
+
+    add_disk(simpledisk);
+
+    printk(KERN_INFO "Init simple block device successfully\n");
+
+    return 0;
+    out2:
+    put_disk(simpledisk);
+    out1:
+    unregister_blkdev(major, "simple_block");
+    return ret;
+}
+
+static void block_exit(void)
+{
+    blk_cleanup_queue(simpledisk->queue);
+    del_gendisk(simpledisk);
+    put_disk(simpledisk);
+    unregister_blkdev(major, "simple_block");
+    printk(KERN_INFO "Goodbye simple disk\n");
+}
+
+module_init(block_init);
+module_exit(block_exit);
